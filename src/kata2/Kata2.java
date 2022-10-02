21a9a4a7942da2ff84fcc9fd0d73dcf249a684a1
santiago140103 <safloresreyna@gmail.com>
2022-10-02 23:15:36 +0100

    Quinta version de la kata 2

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -7,13 +7,13 @@ public class Kata2 {
 
     public static void main(String[] args) {
         
-        Integer[] data = {1, 3, 5, 3, 4, 2, 8, 8, 8, 7, 4, 9, 3, 2, 1};
+        String [] data = {"Hola", "soy", "Santi", "soy", "tengo", "19", "años", "años"};
         
         Histogram histo = new Histogram(data);
         
-        Map<Integer, Integer> histogram = histo.getHistogram();
+        Map<String, Integer> histogram = histo.getHistogram();
         
-        for (int key: histogram.keySet()) {
+        for (String key: histogram.keySet()) {
             System.out.println(key + " ==> " + histogram.get(key));
         }
     }
